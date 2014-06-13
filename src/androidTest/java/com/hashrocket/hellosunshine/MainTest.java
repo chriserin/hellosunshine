c5374d4efe555bb27b6d46807c53f74136f701fb
Chris Erin <chris.erin@gmail.com>
2014-06-13 11:49:58 -0500

    In The Beginning...

---
+++ src/androidTest/java/com/hashrocket/hellosunshine/MainTest.java
@@ -0,0 +1,49 @@
+package com.hashrocket.hellosunshine;
+
+
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.pressBack;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.pressBack;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.closeSoftKeyboard;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class com.hashrocket.hellosunshine.MainTest \
+ * com.hashrocket.hellosunshine.tests/android.test.InstrumentationTestRunner
+ */
+public class MainTest extends ActivityInstrumentationTestCase2<Main> {
+
+    public MainTest() {
+        super("com.hashrocket.hellosunshine", Main.class);
+    }
+
+    @SmallTest
+    public void testSomething() {
+        assertTrue(true);
+    }
+
+    @SmallTest
+    public void testAnother() {
+        onView(allOf(withId(R.id.helloTextView), withText("Hello")));
+    }
+}
